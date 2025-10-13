13b9c5dd84f789979b2ff716cfe226afee25c91f
Maximiliano López <maximiliano.lopez@correo.ucu.edu.uy>
2025-10-13 12:06:31 -0300

    Update Archer.cs

--- src/Library/Characters/Archer.cs
+++ src/Library/Characters/Archer.cs
@@ -1,11 +1,9 @@
 namespace Ucu.Poo.RoleplayGame;
 
-public class Archer : Character
+public class Archer : Character, ICharacter
 {
     public Archer(string name) : base(name)
     {
-        this.Name = name;
-
         this.AddItem(new Bow());
         this.AddItem(new Helmet());
     }
