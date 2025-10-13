eb466ca7f84d6d0afee9952a27e465c4f37fa86b
Maximiliano López <maximiliano.lopez@correo.ucu.edu.uy>
2025-10-13 14:26:45 -0300

    Update Knight.cs

--- src/Library/Characters/Knight.cs
+++ src/Library/Characters/Knight.cs
@@ -4,7 +4,6 @@ namespace Ucu.Poo.RoleplayGame
     {
         public Knight(string name) : base(name)
         {
-            
             this.AddItem(new Sword());
             this.AddItem(new Shield());
             this.AddItem(new Armor());
